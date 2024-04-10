a5cc0c5a27c15db938d431810904fe67ee00d5e8
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-04-10 21:33:40 +0200

    added addShipToBoard() and reciveAttack()

--- main.js
+++ main.js
@@ -17,8 +17,17 @@ function Ship(length) {
 function Gameboard() {
   const boardSize = 10;
   const board = [];
+  let remainingShips = 0;
   for (let i = 0; i < boardSize; i++) {
-    board.push(new Array(boardSize).fill({ canBePlaced: true }));
+    const row = [];
+    for (let j = 0; j < boardSize; j++) {
+      row.push({
+        canBePlaced: true,
+        wasShoot: false,
+        ship: null,
+      });
+    }
+    board.push(row);
   }
   function addShipToBoard(row, column, size, orientation) {
     const ship = Ship(size);
@@ -27,54 +36,78 @@ function Gameboard() {
     }
     if (orientation === "vertical") {
       for (let i = 0; i < size; i++) {
-        board[row][column + i] = ship;
+        board[row][column + i].ship = ship;
         forbidPlaces(row, column + i);
       }
     }
     if (orientation === "horizontal") {
       for (let i = 0; i < size; i++) {
-        board[row + i][column] = ship;
-        forbidPlaces(row + 1, column);
+        board[row + i][column].ship = ship;
+        forbidPlaces(row + i, column);
       }
     }
+    remainingShips++;
+    return true;
   }
   function forbidPlaces(x, y) {
     for (let i = -1; i <= 1; i++) {
       for (let j = -1; j <= 1; j++) {
         if (checkBounds(x + i, y + j)) {
           if (board[x + i][y + j].canBePlaced === true) {
-            board[x + i][y + j] = { canBePlaced: false };
+            board[x + i][y + j].canBePlaced = false;
           }
         }
       }
     }
   }
-
   function checkPlaceValidity(row, column, size, orientation) {
     if (orientation === "horizontal") {
+      for (let i = 0; i < size; i++) {
+        if (!checkBounds(row + i, column)) return false;
+        if (board[row][row + i].canBePlaced === false) return false;
+      }
+    }
+    if (orientation === "vertical") {
       for (let i = 0; i < size; i++) {
         if (!checkBounds(row, column + i)) return false;
         if (board[row][column + i].canBePlaced === false) return false;
       }
     }
-    if (orientation === "vertical") {
-      for (let i = 0; i < size; i++) {
-        if (!checkBounds(row + i, column)) return false;
-        if (board[row + i][column].canBePlaced === false) return false;
-      }
-    }
     return true;
   }
   function checkBounds(x, y) {
+    let result = null;
+    if (x >= 0 && x < boardSize && y >= 0 && y < boardSize) result = true;
+    else {
+      result = false;
+    }
     return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
+  }
+  function reciveAttack(x, y) {
+    board[x][y].wasShoot = true;
+    if (board[x][y].ship) {
+      board[x][y].ship.hit();
+      if (board[x][y].ship.isSunk()) {
+        remainingShips--;
+        return checkRemainingShips();
+      }
+      return true;
+    }
+    return false;
+  }
+  function checkRemainingShips() {
+    if (remainingShips === 0) {
+      return "All ships sunk!";
+    }
+    return true;
   }
   return {
     board,
     addShipToBoard,
+    reciveAttack,
   };
 }
 
 const gameboard = Gameboard();
-gameboard.addShipToBoard(5, 5, 3, "vertical");
-console.log(gameboard.board);
+
 module.exports = { Ship, Gameboard };
--- main.test.js
+++ main.test.js
@@ -20,22 +20,52 @@ test("Ship sunking", () => {
 
 test("Add ship horizontally", () => {
   gameboard.addShipToBoard(3, 3, 2, "horizontal");
-  expect(gameboard.board[1][3]).toStrictEqual({ canBePlaced: true });
-  expect(gameboard.board[2][3]).toStrictEqual({ canBePlaced: false });
-  expect(gameboard.board[3][3].hits).toBe(0);
-  expect(gameboard.board[4][3].hits).toBe(0);
-  expect(gameboard.board[5][3]).toStrictEqual({ canBePlaced: false });
-  expect(gameboard.board[6][3]).toStrictEqual({ canBePlaced: true });
+  expect(gameboard.board[1][3].canBePlaced).toBe(true);
+  expect(gameboard.board[2][3].canBePlaced).toBe(false);
+  expect(gameboard.board[3][3].ship.hits).toBe(0);
+  expect(gameboard.board[4][3].ship.hits).toBe(0);
+  expect(gameboard.board[5][3].canBePlaced).toBe(false);
+  expect(gameboard.board[6][3].canBePlaced).toBe(true);
 });
 
 test("Add ship vertically", () => {
   gameboard.addShipToBoard(5, 5, 3, "vertical");
-  gameboard.board[5][5].hit();
-  expect(gameboard.board[5][3]).toStrictEqual({ canBePlaced: true });
-  expect(gameboard.board[5][4]).toStrictEqual({ canBePlaced: false });
-  expect(gameboard.board[5][5].hits).toBe(1);
-  expect(gameboard.board[5][6].hits).toBe(1);
-  expect(gameboard.board[5][7].hits).toBe(1);
-  expect(gameboard.board[5][8]).toStrictEqual({ canBePlaced: false });
-  expect(gameboard.board[5][9]).toStrictEqual({ canBePlaced: true });
+  gameboard.board[5][5].ship.hit();
+  expect(gameboard.board[5][3].canBePlaced).toBe(true);
+  expect(gameboard.board[5][4].canBePlaced).toBe(false);
+  expect(gameboard.board[5][5].ship.hits).toBe(1);
+  expect(gameboard.board[5][6].ship.hits).toBe(1);
+  expect(gameboard.board[5][7].ship.hits).toBe(1);
+  expect(gameboard.board[5][8].canBePlaced).toBe(false);
+  expect(gameboard.board[5][9].canBePlaced).toBe(true);
+});
+
+test("Add ship with collision", () => {
+  gameboard.addShipToBoard(5, 5, 3, "vertical");
+  expect(gameboard.addShipToBoard(3, 3, 3, "vertical")).toBe(true);
+  expect(gameboard.addShipToBoard(4, 4, 3, "horizontal")).toBe(false);
+});
+
+test("Add ship out of bounds", () => {
+  expect(gameboard.addShipToBoard(0, 0, 3, "horizontal")).toBe(true);
+  expect(gameboard.addShipToBoard(7, 0, 3, "horizontal")).toBe(true);
+  expect(gameboard.addShipToBoard(7, 0, 4, "horizontal")).toBe(false);
+  expect(gameboard.addShipToBoard(0, 7, 1, "vertical")).toBe(true);
+  expect(gameboard.addShipToBoard(2, 9, 2, "vertical")).toBe(false);
+});
+
+test("Shooting at gameboard", () => {
+  gameboard.addShipToBoard(5, 5, 3, "vertical");
+  expect(gameboard.reciveAttack(5, 5)).toBe(true);
+  expect(gameboard.board[5][5].wasShoot).toBe(true);
+  expect(gameboard.board[5][5].ship.hits).toBe(1);
+  expect(gameboard.reciveAttack(1, 2)).toBe(false);
+  expect(gameboard.board[1][2].wasShoot).toBe(true);
+});
+
+test("Sunking ships", () => {
+  gameboard.addShipToBoard(1, 1, 1, "vertical");
+  gameboard.addShipToBoard(1, 6, 1, "vertical");
+  expect(gameboard.reciveAttack(1, 1)).toBe(true);
+  expect(gameboard.reciveAttack(1, 6)).toBe("All ships sunk!");
 });
