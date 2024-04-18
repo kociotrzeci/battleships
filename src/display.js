3fa53986037f956572836394b33d302bceea522e
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-04-18 23:02:53 +0200

    added gui shooting

--- src/display.js
+++ src/display.js
@@ -1,23 +1,33 @@
-function Display(_container, player) {
+function Display(_container, player, mode, otherDisplay) {
   console.log("Display");
   const container = document.getElementById(_container);
   console.log(player);
   for (let i = 0; i < player.gameboard.boardSize; i++) {
     const row = document.createElement("div");
     row.classList.add(`row`);
+
     for (let j = 0; j < player.gameboard.boardSize; j++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
       cell.x = i;
       cell.y = j;
-      cell.addEventListener("click", attack(player));
+      if (mode === "AI") cell.addEventListener("click", attack(player));
+      const dot = document.createElement("span");
+      cell.appendChild(dot);
       row.appendChild(cell);
     }
     container.appendChild(row);
   }
+  refresh();
+
   function attack(player) {
     return function (event) {
       player.getEnemy().shoot(event.currentTarget.x, event.currentTarget.y);
+      refresh();
+      setTimeout(() => {
+        player.shoot();
+        refresh(otherDisplay.refresh());
+      }, 500);
       console.log(
         `${player.getEnemy()} shot at ${event.currentTarget.x}, ${
           event.currentTarget.y
@@ -26,18 +36,22 @@ function Display(_container, player) {
     };
   }
   function refresh() {
-    let i,
+    let i = 0,
       j = 0;
     for (const row of container.children) {
       for (const cell of row.children) {
         cell.classList.remove("black", "white");
-        cell.classList.add(board[i][j]);
+        if (player.gameboard.board[i][j].wasShoot) {
+          cell.classList.add("shoot");
+        }
+        if (player.gameboard.board[i][j].ship) cell.classList.add("ship");
         j++;
       }
       i++;
       j = 0;
     }
   }
+  return { refresh };
 }
 
 module.exports = { Display };
